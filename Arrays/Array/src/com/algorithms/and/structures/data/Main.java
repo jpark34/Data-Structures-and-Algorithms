3642ee54a6a8c16e39c923b9ecf4d04f3bf6fb77
Jarrett Parker <jarrettparker34@gmail.com>
2021-11-19 21:53:47 -0500

    Finished Arrays, Started Sorting Algorithms

--- Arrays/Array/src/com/algorithms/and/structures/data/Main.java
+++ Arrays/Array/src/com/algorithms/and/structures/data/Main.java
@@ -3,6 +3,29 @@ package com.algorithms.and.structures.data;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int[] array = new int[7];
+
+        array[0] = 20;
+        array[1] = 35;
+        array[2] = -15;
+        array[3] = 7;
+        array[4] = 55;
+        array[5] = 1;
+        array[6] = -22;
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        int index = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 7) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("index = " + index);
     }
 }
---
+++ Sort-Algorithms/BubbleSort/src/com/algorithms/and/structures/data/Main.java
@@ -0,0 +1,8 @@
+package com.algorithms.and.structures.data;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
